5bbbdc4979960431df00341943ee87208aeffd29
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-17 16:46:51 -0500

    Scene selector and WebGL build

---
+++ Assets/SceneSelector/SceneSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SceneButton {
+	public string sceneName;
+	public string buttonText;
+}
+
+public class SceneSelector : MonoBehaviour {
+	public GameObject buttonContainer;
+	public GameObject buttonPrefab;
+	public SceneButton[] sceneButtons;
+
+	void Start() {
+		foreach (SceneButton sceneButton in sceneButtons) {
+			GameObject b = Instantiate(buttonPrefab, buttonContainer.transform);
+			b.GetComponentInChildren<Button>().onClick.AddListener(() => { SceneManager.LoadScene(sceneButton.sceneName); });
+			b.GetComponentInChildren<Text>().text = sceneButton.buttonText;
+		}
+	}
+}
